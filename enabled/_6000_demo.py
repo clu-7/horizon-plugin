6b962860f6804328927964b9be05b44ccf650bc8
Cindy Lu <clu@us.ibm.com>
2015-08-19 13:48:38 -0700

    fix reference path to mydashboard

--- enabled/_6000_demo.py
+++ enabled/_6000_demo.py
@@ -17,7 +17,7 @@ DEFAULT = False
 # A dictionary of exception classes to be added to HORIZON['exceptions'].
 ADD_EXCEPTIONS = {}
 # A list of applications to be added to INSTALLED_APPS.
-ADD_INSTALLED_APPS = ['openstack_dashboard.dashboards.mydashboard']
+ADD_INSTALLED_APPS = ['mydashboard']
 
 ADD_ANGULAR_MODULES = [
     'horizon.dashboard.mydashboard',
